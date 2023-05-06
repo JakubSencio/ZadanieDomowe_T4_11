13e580b2386fe2fadadbb4ff93404d80577a7405
JacobKomp <sencio.jakub@Gmail.com>
2023-05-07 00:11:40 +0200

    Dodaj pliki projektów.

---
+++ ZadanieDomowe_T4_11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieDomowe_T4_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program zwrot = new Program();
+                Console.WriteLine("Wprowadz liczbe, ktorej parzystosc chcesz sprawdzic");
+                var numberToCheck = int.Parse(Console.ReadLine());
+
+                var wynik = zwrot.zwrot(numberToCheck, 2);
+
+                if (wynik == 1)
+                {
+                    Console.WriteLine("Podana liczba jest nieparzysta!");
+                }
+                else if (wynik == 0)
+                {
+                    Console.WriteLine("Podana liczba jest parzysta!");
+                }
+            }
+            catch(Exception)
+            {
+                throw new Exception("\nBledne dane!");
+            }
+        }
+        public int zwrot(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
